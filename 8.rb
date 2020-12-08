ca3792782812e75b7ee7741f645d3e8c11804056
tmokmss <tomookam@live.jp>
2020-12-09 08:57:36 +0900

    refactor

--- 8.rb
+++ 8.rb
@@ -2,29 +2,35 @@ require 'set'
 input = File.read('input8.txt').split("\n")
 
 class Op
-    attr_reader :op, :sign, :val
+    attr_reader :op, :val
 
-    def initialize(op, sign, val)
+    def initialize(op, val)
         @op = op
-        @sign = sign
         @val = val.to_i
     end
 
     def to_s
-        "#{self.op} #{self.sign} #{self.val}"
+        "#{self.op} #{self.val}"
+    end
+
+    def flip!
+        if @op == 'nop'
+            @op = 'jmp'
+        elsif @op == 'jmp'
+            @op = 'nop'
+        end
     end
 end
 
 ops = input.map do |line|
     args = line.split
     op = args[0]
-    sign = args[1][0]
-    val = args[1][1..]
-    Op.new(op, sign, val)
+    val = args[1]
+    Op.new(op, val)
 end
 
 
-def ends?(ops, switch)
+def ends?(ops)
     executed = Set.new
     curr = 0
     acc = 0
@@ -33,21 +39,13 @@ def ends?(ops, switch)
         break if executed.add?(curr) == nil
 
         op = ops[curr]
-        operator = op.op
-        if switch == curr
-            if operator == 'nop'
-                operator = 'jmp'
-            elsif operator == 'jmp'
-                operator = 'nop'
-            end
-        end
 
-        case operator
+        case op.op
         when 'nop'
         when 'acc'
-            acc = acc.send(op.sign, op.val)
+            acc += op.val
         when 'jmp'
-            curr = curr.send(op.sign, op.val)
+            curr += op.val
             next
         end
         curr += 1
@@ -56,12 +54,13 @@ def ends?(ops, switch)
     [acc, curr >= ops.size]
 end
 
-acc, _ = ends?(ops, -1)
+acc, _ = ends?(ops)
 puts(acc)
 
 (0..ops.size).each do |i|
-    acc, completed = ends?(ops, i)
-    # puts("#{i} - #{acc} #{completed}")
+    ops[i].flip!
+    acc, completed = ends?(ops)
+    ops[i].flip!
     break if completed
 end
 
