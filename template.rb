485291441ec5c2b31ad22427693274f424c3bf18
tmokmss <tomookam@live.jp>
2020-12-09 17:08:06 +0900

    update template

--- template.rb
+++ template.rb
@@ -1,8 +1,14 @@
 require 'set'
-input = File.read('input9.txt').split("\n")
+input = File.read('input.txt').split("\n")
 
-input.each do |line|
+ans = 0
+
+input.each_with_index do |line, i|
     
+end
+
+(0...input.size).each do |i|
+    input[i]
 end
 
 puts(ans)
