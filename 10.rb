b6cec8e63eac845d376d22b7a5fe590352815dcd
tmokmss <tomookam@live.jp>
2020-12-10 15:18:14 +0900

    refactor

--- 10.rb
+++ 10.rb
@@ -1,29 +1,25 @@
-require 'set'
-input = File.read('input10.txt').split("\n").map(&:to_i).sort!
+input = File.read('input10.txt').split("\n").map(&:to_i)
+input.push(0)
+input.push(input.max+3)
+input.sort!
+n = input.size
 
-ans = 0
-adapter = input.last + 3
+adapter = input.last
 
 count = Hash.new(0)
-last = 0
-input.each_with_index do |line, i|
-    count[line - last] += 1
-    last = line
+(0...n-1).each do |i|
+    count[input[i+1] - input[i]] += 1
 end
-count[3] += 1
 
 puts(count[1] * count[3])
 
-n = input.size
-dp = Array.new(n+2, 0)
-dp[n+1] = 1
-input.unshift(0)
-input.push(adapter)
-(0...n+1).each do |i|
-    j = n - i
+dp = Array.new(n, 0)
+dp[n-1] = 1
+(1...n).each do |i|
+    j = n - i - 1
     v = input[j]
     (1..3).each do |k|
-        break if j+k > n+1
+        break if j+k >= n
         if (input[j+k] <= v + 3)
             dp[j] += dp[j+k]
         else 
@@ -32,7 +28,7 @@ input.push(adapter)
     end
 end
 
-(0..n+1).each do |i|
+(0...n).each do |i|
     # puts "#{input[i]}, #{dp[i]}"
 end
 puts dp[0]
