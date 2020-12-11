398770d000ba9d671307b5dc2832169ab3f6f655
tmokmss <tomookam@live.jp>
2020-12-11 13:26:39 +0900

    add variable n

--- template.rb
+++ template.rb
@@ -1,13 +1,14 @@
 require 'set'
 input = File.read('input.txt').split("\n")
 
+n = input.size
 ans = 0
 
 input.each_with_index do |line, i|
     
 end
 
-(0...input.size).each do |i|
+(0...n).each do |i|
     input[i]
 end
 
