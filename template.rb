306a60758a4051988c88754d1099cd57628741ca
tmokmss <tomookam@live.jp>
2020-12-16 14:41:45 +0900

    add regex matching sample

--- template.rb
+++ template.rb
@@ -9,6 +9,10 @@ end
 puts "Load input from #{input_path}"
 
 input = File.read(input_path).split("\n").map(&:strip)
+parsed = input.map do |line|
+  match = /mem\[(.*)\] = (.*)/.match(line)
+  [match[1], match[2]]
+end
 
 n = input.size
 ans = 0
